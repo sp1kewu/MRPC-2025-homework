2aec6a02d834ed01f02cf016cdb90809d53d5f4c
sp1kewu <2650126300@qq.com>
2025-12-03 09:31:07 -0800

    test:114514

--- code/src/quadrotor_simulator/so3_control/src/so3_control_nodelet.cpp
+++ code/src/quadrotor_simulator/so3_control/src/so3_control_nodelet.cpp
@@ -100,8 +100,8 @@ SO3ControlNodelet::position_cmd_callback(
   des_acc_ = Eigen::Vector3d(cmd->acceleration.x, cmd->acceleration.y,
                              cmd->acceleration.z);
 
-  kx_ = Eigen::Vector3d(15,9,6);
-  kv_ = Eigen::Vector3d(10,6,4);
+  kx_ = Eigen::Vector3d(1,1,4);
+  kv_ = Eigen::Vector3d(5,1,4);
 
   des_yaw_              = cmd->yaw;
   des_yaw_dot_          = cmd->yaw_dot;
